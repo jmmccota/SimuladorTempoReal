02a512389b7a3f43aa65cfa3244ee6f7f37bd904
jmmccota <joao_marcos_cota@hotmail.com>
2015-02-05 18:05:25 -0200

    Simuladortemporeal2 alterações terminadas

--- src/simuladortemporeal/DM.java
+++ src/simuladortemporeal/DM.java
@@ -16,25 +16,20 @@ public class DM {
     ArrayList<Processo> list;
 
     public DM(ArrayList<Processo> lista) {
-        list=lista;
+        list = lista;
     }
-    
-    
-    
-    
-    public void atualizaOrdem(int tempo){
+
+    public void atualizaOrdem(int tempo) {
         Processo aux;
         for (int i = 0; i < list.size(); i++) {
             for (int j = i + 1; j < list.size(); j++) // sempre 1 elemento à frente
             {
-                if ((ordem.get(i).getPeriodo()-tempo) > (ordem.get(j).getPeriodo()-tempo)) {
-                   aux=ordem.get(i);
-                   ordem.set(i, ordem.get(j));
-                   ordem.set(j, aux);
-                   
-                }
+//                if ((ordem.get(i).getPeriodo()-tempo) > (ordem.get(j).getPeriodo()-tempo)) {
+//                   aux=ordem.get(i);
+//                   ordem.set(i, ordem.get(j));
+//                   ordem.set(j, aux);
+//                   
             }
-        } // fim
-    }
-
+        }
+    } // fim
 }
--- src/simuladortemporeal/EDF.java
+++ src/simuladortemporeal/EDF.java
@@ -29,12 +29,12 @@ public class EDF {
         for (int i = 0; i < list.size(); i++) {
             for (int j = i + 1; j < list.size(); j++) // sempre 1 elemento à frente
             {
-                if ((ordem.get(i).getPeriodo()-tempo) > (ordem.get(j).getPeriodo()-tempo)) {
-                   aux=ordem.get(i);
-                   ordem.set(i, ordem.get(j));
-                   ordem.set(j, aux);
-                   
-                }
+//                if ((ordem.get(i).getPeriodo()-tempo) > (ordem.get(j).getPeriodo()-tempo)) {
+//                   aux=ordem.get(i);
+//                   ordem.set(i, ordem.get(j));
+//                   ordem.set(j, aux);
+//                   
+//                }
             }
         } // fim
     }
